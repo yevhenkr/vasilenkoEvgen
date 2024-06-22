54dedc693d49efec50433b59a56fa191de23d22b
Eugen <vasilenkoevgen88@gmail.com>
2024-06-22 13:25:33 +0300

    refactor: typeScript

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import * as path from "path";
 
-import * as path from 'path'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  css: {
+    modules: {
+      localsConvention: "camelCase", // Опционально: преобразование имен классов в camelCase
+    },
+  },
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
-})
+});
