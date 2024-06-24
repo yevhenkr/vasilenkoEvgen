ca19cd64368ababd3bde20ce7f3173511c2a1725
Eugen <vasilenkoevgen88@gmail.com>
2024-06-25 02:29:34 +0300

    feat: add icon

---
+++ src/assets/icons/htmlIcon.tsx
@@ -0,0 +1,13 @@
+import { memo } from 'react'
+
+export const HtmlIcon = memo(() => (
+  <svg height={'50'} viewBox={'0 0 20 20'} xmlns={'http://www.w3.org/2000/svg'}>
+    <path
+      d={
+        'm1.5 0l1.547 18l6.943 2l6.961-2.002L18.5 0zm13.773 4.272l-.097 1.119l-.043.496H6.99l.195 2.26h7.755l-.052.594l-.5 5.812l-.032.373L10 16.178l-.01.004l-4.36-1.256l-.297-3.467h2.136l.151 1.762l2.37.663h.002l2.374-.665l.247-2.863H5.237l-.523-6.084l-.05-.593h10.66z'
+      }
+      fill={'var(--color-info-700)'}
+      fillRule={'evenodd'}
+    ></path>
+  </svg>
+))
