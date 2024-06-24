222ad66117a0e74ab6f03e30c81f8c3d62750059
Eugen <vasilenkoevgen88@gmail.com>
2024-06-25 00:58:25 +0300

    fix: style background

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,16 @@
 import { Main } from '@/common/components/ui/main/Main'
+import { Skills } from '@/common/components/ui/skills/Skills'
+
+import s from './app.module.scss'
 
 import { Header } from '../common/components/ui/header/Header'
 
 export function App() {
   return (
-    <div>
+    <div className={s.app}>
       <Header />
       <Main />
+      <Skills />
     </div>
   )
 }
