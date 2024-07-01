a3ccab9c56922f8c35e2433dcc24837cd7979f7b
Eugen <vasilenkoevgen88@gmail.com>
2024-07-01 23:00:12 +0300

    refactor:  styled

--- src/common/components/ui/header/Header.tsx
+++ src/common/components/ui/header/Header.tsx
@@ -1,13 +1,26 @@
 import { Menu } from '@/common/components/ui/menu/Menu'
-
-import s from '@/common/components/ui/header/header.module.scss'
+import styled from 'styled-components'
 
 export const Header = () => {
   return (
-    <header className={s.root}>
-      <div>
-        <Menu />
-      </div>
-    </header>
+    <StyledHeader>
+      <Menu />
+    </StyledHeader>
   )
 }
+
+const StyledHeader = styled.header`
+  position: fixed;
+  z-index: 2;
+  top: 0;
+  right: 0;
+  left: 0;
+
+  width: 100%;
+  height: var(--header-height);
+  min-height: 50px;
+  padding: 12px 16px;
+
+  background: var(--color-dark-700);
+  border-bottom: 1px solid var(--color-dark-500);
+`
