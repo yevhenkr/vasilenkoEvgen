30c6bb0d4ddaedb506b666c3f7e41009fa064130
Eugen <vasilenkoevgen88@gmail.com>
2024-07-01 23:50:06 +0300

    refactor: ICON new link

--- src/common/components/ui/footer/footerSocialMedia/footerSocialMedia.tsx
+++ src/common/components/ui/footer/footerSocialMedia/footerSocialMedia.tsx
@@ -4,11 +4,25 @@ import styled from 'styled-components'
 const FooterSocialMedia = () => {
   return (
     <StyleDiv>
-      <Icon iconId={'telegram'} />
-      <Icon iconId={'linkedIn'} />
-      <Icon iconId={'codeWars'} />
-      <Icon iconId={'gitHub'} />
-      <Icon iconId={'email1'} iconId2={'email2'} viewBox={'0 0 60 60'} />
+      <a href={'https://t.me/VasilenkoEvgen'} rel={'noreferrer'} target={'_blank'}>
+        <Icon iconId={'telegram'} />
+      </a>
+      <a
+        href={'https://www.linkedin.com/in/yevhen-vasilenko-0a2171161/'}
+        rel={'noreferrer'}
+        target={'_blank'}
+      >
+        <Icon iconId={'linkedIn'} />
+      </a>{' '}
+      <a href={'https://www.codewars.com/users/yevhenkr'} rel={'noreferrer'} target={'_blank'}>
+        <Icon iconId={'codeWars'} />
+      </a>
+      <a href={'https://github.com/yevhenkr'} rel={'noreferrer'} target={'_blank'}>
+        <Icon iconId={'gitHub'} />{' '}
+      </a>
+      <a href={'mailto:vasilenkoevgen88@gmail.com'} rel={'noreferrer'} target={'_blank'}>
+        <Icon iconId={'email1'} iconId2={'email2'} viewBox={'0 0 60 60'} />{' '}
+      </a>
     </StyleDiv>
   )
 }
