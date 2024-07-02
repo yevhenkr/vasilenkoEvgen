44943e2d2626436fddb36ac1fb4e20e0c5b1f943
Eugen <vasilenkoevgen88@gmail.com>
2024-07-02 14:25:21 +0300

    refactor: flex wrap

--- src/common/components/ui/flexWrapper/FlexWrapper.tsx
+++ src/common/components/ui/flexWrapper/FlexWrapper.tsx
@@ -1,16 +1,22 @@
 import styled from 'styled-components'
 
 type FlexWrapperType = {
-  alignI?: string
-  flexDirection?: string
-  flexWrap?: string
-  justifyC?: string
+  align_i?: 'baseline' | 'center' | 'flex-end' | 'flex-start' | 'stretch'
+  flexDirection?: 'column' | 'column-reverse' | 'row' | 'row-reverse'
+  gap?: string
+  justify?: 'center' | 'flex-end' | 'flex-start' | 'space-around' | 'space-between' | 'space-evenly'
+  wrap?: 'nowrap' | 'wrap' | 'wrap-reverse'
 }
 
 export const FlexWrapper = styled.div<FlexWrapperType>`
   display: flex;
   flex-direction: ${props => props.flexDirection || 'row'};
-  justify-content: ${props => props.justifyC || 'flex-start'};
-  align-items: ${props => props.alignI || 'stretch'};
-  flex-wrap: ${props => props.flexWrap || 'nowrap'};
+  justify-content: ${props => props.justify || 'flex-start'};
+  align-items: ${props => {
+    console.log(props.align_i)
+
+    return props.align_i || 'stretch'
+  }};
+  flex-wrap: ${props => props.wrap || 'nowrap'};
+  gap: ${props => props.gap || '10px'};
 `
